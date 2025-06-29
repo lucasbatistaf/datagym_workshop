3eb121884fd9b26eb9adf6f9e8ba49199ac52e74
lucasbatistaf <lucaasbatista182@gmail.com>
2025-06-28 21:50:55 -0300

    staging finished

---
+++ horrorland/models/staging/people/stg_people__customers.sql
@@ -0,0 +1,19 @@
+with
+    source as (
+        select
+            customer_id,
+            age,
+            case
+                when gender in ('M', 'Male') then 'M'
+                when gender in ('F', 'Female') then 'F'
+                else 'Other' 
+            end as gender,
+            email,
+            registration_date,
+            to_boolean(lower(is_vip_member)) as is_vip_member,
+            preferred_scare_level,
+        from {{ source('PEOPLE', 'CUSTOMERS') }}
+    )
+
+select *
+from source
---
+++ logs/query_log.sql
@@ -0,0 +1,5 @@
+-- created_at: 2025-06-28T23:26:06.421378683+00:00
+-- dialect: snowflake
+-- node_id: not available
+-- desc: not provided
+select 1 as id;
